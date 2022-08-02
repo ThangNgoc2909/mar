d4d43b7a03f2c5917fc4d5be3af480b28ca8642e
Thang Ngoc <thangngoc2909@gmail.com>
2022-08-02 16:47:00 +0700

    sendSMS

--- helper/otp_verification.js
+++ helper/otp_verification.js
@@ -1,17 +1,19 @@
-const otpGen = require('otp-generator');
-const crypto = require('crypto')
+const crypto = require('crypto');
+const { sendSMS } = require('../helper/send_message')
 const key = "otp-secret-key"
 
+const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN } = process.env 
+
 exports.createOtp = async (params, callback) => {
-    const otp = otpGen.generate(4, {
-        specialChars: false,
-        upperCaseAlphabets: false,
-    })
+    const otp = Math.floor(1000 + Math.random() * 9000);
+    const bodySMS = `Your OTP is ${otp}`
     const ttl = 5 * 60 * 1000
     const expireDate = Date.now() + ttl;
     const data = `${params.phone}.${otp}.${expireDate}`
-    console.log(otp);
     const hash = crypto.createHmac("sha256", key).update(data).digest("hex")
+    const phoneNumber = params.phone.replace('0', '+84')
+    console.log('phoneNumber', phoneNumber);
+    sendSMS(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, bodySMS, phoneNumber)
     const fullHash = `${hash}.${expireDate}`
     return callback(null, fullHash)
 }
@@ -20,7 +22,7 @@ exports.verifyOtp = async (params, callback) => {
     let [hashValue, expires] = params.hash.split('.');
     console.log(expires);
     let now = Date.now()
-    if (now > parseInt(expires)) return callback("OTP Expired") 
+    if (now > parseInt(expires)) return callback("OTP Expired")
     let data = `${params.phone}.${params.otp}.${expires}`
     let newCalculateHash = crypto.createHmac("sha256", key).update(data).digest("hex")
     if (newCalculateHash === hashValue) {
--- helper/send_message.js
+++ helper/send_message.js
@@ -1,20 +1,12 @@
-const Vonage = require('@vonage/server-sdk');
-const vonage = new Vonage({
-  apiKey: "8cd4842f",
-  apiSecret: "8AZi730Cq2BZfIBZ"
-});
+const twilio = require('twilio');
 
-const from = "Thang Nguyen"
-const to = "84363757636"
-
-exports.sendSMS = vonage.message.sendSms(from, to, text, (err, responseData) => {
-    if (err) {
-        console.log(err);
-    } else {
-        if(responseData.messages[0]['status'] === "0") {
-            console.log("Message sent successfully.");
-        } else {
-            console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
-        }
-    }
-})
+exports.sendSMS = (accountSid, authToken, body, to) => {
+  const client = new twilio(accountSid, authToken);
+  return client.messages
+  .create({
+    body: body,
+    to: to, 
+    from: '+18043363492',
+  })
+  .then((message) => console.log(message.sid));
+}
